a9e6f3484c5fad684f4b7fb83bf7550fad5a480f
Neelima-Evan <neelima.bellary@gmail.com>
2019-07-26 00:58:28 +0530

    This is my first commit

---
+++ src/test/java/testCases/TC_LoginDDT_002.java
@@ -0,0 +1,98 @@
+package testCases;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.NoAlertPresentException;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
+import testCases.BaseClass;
+import utilities.XLUtils;
+
+public class TC_LoginDDT_002 extends BaseClass{
+	
+	@Test(dataProvider="LoginData")
+	public void loginDDT(String user, String pwd) throws InterruptedException
+	{
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName(user);
+		logger.info("user name provided");
+		lp.setPassword(pwd);
+		logger.info("password provided");
+		
+		lp.clickSubmit();
+		
+		logger.info("Clicked on login button");
+		Thread.sleep(3000);
+		boolean ans=isAlertPresent();
+		
+		if(ans)
+		{
+			driver.switchTo().alert().accept();
+			driver.switchTo().defaultContent();
+			Assert.assertTrue(false);
+			logger.warn("Login Failed");
+		}else {
+			Assert.assertTrue(true);
+			logger.info("Login Passed");
+			lp.clickLogout(); 
+			Thread.sleep(3000);
+			driver.switchTo().alert().accept();
+			driver.switchTo().defaultContent();
+		}
+		
+		
+	}
+	
+	public boolean isAlertPresent()
+	{
+		try {
+		driver.switchTo().alert();
+		return true;
+		}
+		catch(NoAlertPresentException e)
+		{
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@DataProvider(name="LoginData")
+	String [][] getData() throws IOException
+	{
+		
+		String path=System.getProperty("user.dir")+
+				"/src/test/java/testData/LoginData.xlsx";
+		int rownum=XLUtils.getRowCount(path,  "Sheet1");
+		int colcount=XLUtils.getCellCount(path, "Sheet1", 1);
+		
+		String logindata[][]=new String [rownum][colcount];
+		
+		for(int i=1;i<=rownum;i++)
+		{
+			for(int j=0; j<colcount; j++)
+			{
+				logindata[i-1][ j]=XLUtils.getCellData(path, "Sheet1", i, j);
+			}
+		}
+		
+		return 	logindata;	
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
